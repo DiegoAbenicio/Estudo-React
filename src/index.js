314a59de684b9796d969d4576a6499f92a5d390b
Diego Alípio Abenicio <diego.abenicio1@gmail.com>
2023-05-01 23:51:18 -0300

    Using Hooks

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Counter extends React.Component {
-  state = {
-    counter: 0,
-  };
-  increment = () => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-  render() {
-    return (
-      <div>
-        <p>{this.state.counter}</p>
-        <button onClick={this.increment}>Increment</button>
-      </div>
-    );
+function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
   }
+  return (
+    <div>
+      <p>{counter}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
 }
 
 const el = <Counter />;
