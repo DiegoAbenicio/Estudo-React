000713da946ccec29a70f383e61b3516f054a197
Diego Alípio Abenicio <diego.abenicio1@gmail.com>
2023-05-30 21:49:47 -0300

    Task

--- src/App.js
+++ src/App.js
@@ -1,44 +1,25 @@
+import React from "react";
 import "./App.css";
 //import Card from "./Card.js";
-// code to understand the Prop drilling
+import Fruits from "./Fruits";
+import FruitsCounter from "./FruitsCounter";
 
-function Main(props) { 
-  return <Header msg={props.msg} />; 
-};
+function App() {
+  const [fruits] = React.useState([
+    { fruitName: "apple", id: 1 },
+    { fruitName: "apple", id: 2 },
+    { fruitName: "plum", id: 3 },
+    { fruitName: "amora", id: 4 },
+  ]);
 
-function Header(props) { 
-  return ( 
-    <div style={{ border: "10px solid whitesmoke" }}> 
-      <h1>Header here</h1> 
-      <Wrapper msg={props.msg} /> 
-    </div> 
-  ); 
-};
 
-function Wrapper(props) { 
-  return ( 
-    <div style={{ border: "10px solid lightgray" }}> 
-      <h2>Wrapper here</h2> 
-      <Button msg={props.msg} /> 
-    </div> 
-  ); 
-};
-
-function Button(props) { 
-  return ( 
-    <div style={{ border: "20px solid orange" }}> 
-      <h3>This is the Button component</h3> 
-      <button onClick={() => alert(props.msg)}>Click me!</button> 
-    </div> 
-  ); 
-};
-
-function App() { 
-  return ( 
-    <Main  
-      msg="I passed through the Header and the Wrapper and I reached the Button component"  
-    /> 
-  ); 
-}; 
+  return (
+    <div className="App">
+      <h1>Where should the state go?</h1>
+      <Fruits fruits={fruits} />
+      <FruitsCounter fruits={fruits} />
+    </div>
+  );
+}
 
 export default App;
