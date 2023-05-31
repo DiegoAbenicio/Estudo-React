d50ff9d8c700d1226f833b19f61b73836c6fa558
Diego Alípio Abenicio <diego.abenicio1@gmail.com>
2023-05-30 21:18:41 -0300

    Prop drilling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import "./App.css";
 //import Card from "./Card.js";
+// code to understand the Prop drilling
 
-function App() {
+function Main(props) { 
+  return <Header msg={props.msg} />; 
+};
 
-  function handleClick() {
-    let randomNum = Math.floor(Math.random() * 3) + 1;
-    console.log(randomNum);
-    let userInput = prompt("Digite um numero");
-    alert(`Número computado: ${randomNum}, Seu chute: ${userInput}`);
-  }
+function Header(props) { 
+  return ( 
+    <div style={{ border: "10px solid whitesmoke" }}> 
+      <h1>Header here</h1> 
+      <Wrapper msg={props.msg} /> 
+    </div> 
+  ); 
+};
 
-  return (
-    <div>
-      <h1>Tarefa: Adicione um botao e um evento</h1>
-      <button onClick={handleClick}>Chute um número de 1 a 3</button>
-    </div>
-  );
-}
+function Wrapper(props) { 
+  return ( 
+    <div style={{ border: "10px solid lightgray" }}> 
+      <h2>Wrapper here</h2> 
+      <Button msg={props.msg} /> 
+    </div> 
+  ); 
+};
+
+function Button(props) { 
+  return ( 
+    <div style={{ border: "20px solid orange" }}> 
+      <h3>This is the Button component</h3> 
+      <button onClick={() => alert(props.msg)}>Click me!</button> 
+    </div> 
+  ); 
+};
+
+function App() { 
+  return ( 
+    <Main  
+      msg="I passed through the Header and the Wrapper and I reached the Button component"  
+    /> 
+  ); 
+}; 
 
 export default App;
